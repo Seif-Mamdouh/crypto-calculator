7fa1c360e5e47a93086d682b8e55c2a49106e50f
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-01-05 20:05:48 -0500

    backend changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "./style.css";
 //For this, we will be using the useState hook.
 
 export default function App() {
-  const [buy, setBuy] = useState("");
+  const [buy, setBuyDate] = useState("");
+  const [sell, setSellDate] = useState("");
 
   const coingeckoUrl = (date) => {
     return `https://api.coingecko.com/api/v3/coins/bitcoin/history?date=${date}&localization=false`;
@@ -13,18 +14,84 @@ export default function App() {
   const coingeckoFetch = async (date) => {
     fetch(coingeckoUrl(date)).then((response) =>
       response.json().then((jsonData) => {
-        setBuy(jsonData);
+        setBuyDate(jsonData);
+        setSellDate(jsonData);
         console.log(jsonData);
       })
     );
   };
 
+  const handleBuyChange = (e) => {
+    let val = e.target.value;
+    setBuyDate(val);
+    coingeckoFetch(val);
+  };
+
+  const handleSellChange = (e) => {
+    let val = e.target.value;
+    setBuyDate(val);
+    coingeckoFetch(val);
+
+  }
+
   return (
     <div className="App">
-      <input defaultValue={buy} onChange={(e) => setBuy(e.target.value)}/>
-      <h3> {buy.market_data?.current_price.usd} USD</h3>
+      <input defaultValue={buyDate} onChange={(val) => handleBuyChange(val)} />
+      <h3> {buyDate.market_data?.current_price.usd} USD</h3>
+
+      <input
+        defaultValue={sellDate}
+        onChange={(val) => handleSellChange(val)}
+      />
+      <h3> {sellDate.market_data?.current_price.usd} USD</h3>
     </div>
   );
 }
 
 // export default App;
+
+
+// import React, { useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   const [buyDate, setBuyDate] = useState("");
+//   const [sellDate, setSellDate] = useState("");
+
+//   const coingeckoUrl = (date) => {
+//     return `https://api.coingecko.com/api/v3/coins/bitcoin/history?date=${date}&localization=false`;
+//   };
+
+//   const coingeckoFetch = async (date) => {
+//     fetch(coingeckoUrl(date)).then((response) =>
+//       response.json().then((jsonData) => {
+//         setBuyDate(jsonData);
+//         setSellDate(jsonData);
+//         console.log(jsonData);
+//       })
+//     );
+//   };
+//   const handleBuyChange = (e) => {
+//     let val = e.target.value;
+//     setBuyDate(val);
+//     coingeckoFetch(val);
+//   };
+//   const handleSellChange = (e) => {
+//     let val = e.target.value;
+//     setBuyDate(val);
+//     coingeckoFetch(val);
+//   };
+
+//   return (
+//     <div className="App">
+//       <input defaultValue={buyDate} onChange={(val) => handleBuyChange(val)} />
+//       <h3> {buyDate.market_data?.current_price.usd} USD</h3>
+
+//       <input
+//         defaultValue={sellDate}
+//         onChange={(val) => handleSellChange(val)}
+//       />
+//       <h3> {sellDate.market_data?.current_price.usd} USD</h3>
+//     </div>
+//   );
+// }
